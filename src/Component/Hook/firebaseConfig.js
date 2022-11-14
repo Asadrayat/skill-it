31a355bf15819b8638115fc956bc15ebadefaa2e
Asadrayat <asad.rayat4@gmail.com>
2022-11-14 11:48:13 +0600

    env added

--- src/Component/Hook/firebaseConfig.js
+++ src/Component/Hook/firebaseConfig.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBNwXlM6O39XlFM_FYI8AfFGZKdEvzG3LI",
-  authDomain: "skill-it-be278.firebaseapp.com",
-  projectId: "skill-it-be278",
-  storageBucket: "skill-it-be278.appspot.com",
-  messagingSenderId: "1038412981775",
-  appId: "1:1038412981775:web:71a15d1d7957706bc0a57a"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
